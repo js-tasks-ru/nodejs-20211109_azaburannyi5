7ad284bcc45140594cd632bf02bf25cdb0cdae9f
a.zaburannyi <a.zaburannyi@a.zaburannyi>
2021-11-24 16:37:18 +0200

    solution for 3-module 2-task

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,35 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.line = "";
   }
 
   _transform(chunk, encoding, callback) {
+
+    let str = chunk.toString(this.encoding);
+
+    str.split("").forEach((symbol) => {
+      if (symbol == os.EOL) {
+
+        this.push(this.line);
+
+        this.line = "";
+
+      } else {
+        this.line += symbol
+      }
+      
+    });
+
+    callback();
   }
 
   _flush(callback) {
+
+    this.push(this.line);
+
+    callback();    
   }
 }
 
