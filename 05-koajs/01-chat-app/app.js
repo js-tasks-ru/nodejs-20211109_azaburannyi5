00adf090d029425ae6cec82955b791b668c813e0
a.zaburannyi <a.zaburannyi@a.zaburannyi>
2021-12-06 22:29:09 +0200

    solution for 5-module 1-task

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,10 +8,42 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let subscribers = {};
+
 router.get('/subscribe', async (ctx, next) => {
+
+    let id = Math.random();
+
+    subscribers[id] = null;
+
+    ctx.body = await new Promise(resolve => {
+        let interval = setInterval(() => {
+            if (subscribers[id] != null) {
+                let message = subscribers[id];
+                clearInterval(interval);
+                delete subscribers[id]
+
+                resolve(message)
+            }
+
+        }, 10)
+    });
+
+    return next();
+
 });
 
 router.post('/publish', async (ctx, next) => {
+
+    if (ctx.request.body.message) {
+        for (const [key, value] of Object.entries(subscribers)) {
+            subscribers[key] = ctx.request.body.message;
+        }
+    }
+
+    ctx.body = ctx.request.body.message;
+
+    return next();
 });
 
 app.use(router.routes());
