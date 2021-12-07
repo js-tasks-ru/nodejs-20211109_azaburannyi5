bd3ee16c67a186c6e692cd5ca3366bc4a2170978
a.zaburannyi <a.zaburannyi@a.zaburannyi>
2021-12-07 17:45:43 +0200

    solution for 6-module 3-task

--- 06-mongodb-mongoose/03-search/controllers/products.js
+++ 06-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,20 @@
+const productMapper = require('../../02-rest-api/mappers/product');
+const Product = require('../models/Product');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
+
+  let {query} = ctx.query;
+
+  let products = await Product.find({ $text : { $search : query }})
+
+  if (products.length != 0) {
+    
+    ctx.body = {products: products.map(product => productMapper(product))};
+
+    return next();
+  }
+
   ctx.body = {products: []};
+
+  return next();
 };
