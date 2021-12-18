62e33f59e7443f296950ca6a6df3e790526430c2
a.zaburannyi <a.zaburannyi@pls.life>
2021-12-18 21:02:44 +0200

    solution for 08-module 02-task

--- 08-email/02-order/models/Order.js
+++ 08-email/02-order/models/Order.js
@@ -2,6 +2,37 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Product',
+    },
+    phone: {
+        type: String,
+        required: true,
+        validate: [
+            {
+                validator(value) {
+                return /\+?\d{6,14}/.test(value);
+                },
+                message: 'Неверный формат номера телефона.',
+            },
+        ],
+    },
+    address: {
+        type: String,
+        required: true
+    }
+});
+
+orderSchema.set('toJSON', {
+    virtuals: true
 });
 
 module.exports = connection.model('Order', orderSchema);
+
